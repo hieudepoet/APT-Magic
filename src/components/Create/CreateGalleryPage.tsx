1174f34b7ae62329a896bd74daf3a69a6816b078
Viet Hieu Dinh <59277071+hieudepoet@users.noreply.github.com>
2025-10-17 06:14:35 +0700

    init: Create CreateGalleryPage.tsx

---
+++ src/components/Create/CreateGalleryPage.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react";
+import TransformInterface from "@/components/Create/TransformInterface";
+import FeaturesSection from "@/components/Create/FeaturesSection";
+
+export default function CreateGalleryPage() {
+  return (
+    <div className="min-h-screen relative overflow-hidden pt-16 text-center">
+      {/* Context */}
+      <div className="relative z-10 pt-24 pb-12">
+        <FeaturesSection />
+      </div>
+    </div>
+  );
+}
