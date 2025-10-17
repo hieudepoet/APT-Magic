e48d3cc88d1b624508ef22ac6e36253347e9d5fe
Viet Hieu Dinh <59277071+hieudepoet@users.noreply.github.com>
2025-10-17 07:14:41 +0700

    init: Global loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingSpinner from "@/components/Common/LoadingSpinner";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <LoadingSpinner />
+    </div>
+  );
+}
